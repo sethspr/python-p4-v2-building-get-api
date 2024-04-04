2c7e67113a0b241a400fdca725628680d0c1b264
seth spring <seth.c.spring@gmail.com>
2024-04-04 16:23:07 -0400

    working

--- server/app.py
+++ server/app.py
@@ -20,6 +20,42 @@ def index():
     return "Index for Game/Review/User API"
 
 # start building your API here
+@app.route('/games')
+def games():
+
+    games = []
+    for game in Game.query.all():
+        game_dict = game.to_dict()
+        games.append(game_dict)
+
+    # response = make_response(
+    #     jsonify(games),
+    #     200
+    # )
+
+    # return response
+    return games, 200
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    if game == None:
+        return {"Error": "Game not found"}, 404
+
+    game_dict = game.to_dict()
+
+    return game_dict, 200
+
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    users = []
+    for user in game.users:
+        user_dict = user.to_dict(rules=("-reviews",))
+        users.append(user_dict)
+
+    return users, 200
 
 
 if __name__ == '__main__':
